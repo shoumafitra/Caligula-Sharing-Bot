20a06e57d885950052a3b2d71d6a02975c2eb0a9
shoumafitra <87926456+shoumafitra@users.noreply.github.com>
2021-08-17 13:06:44 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/shoumafitra/File-Sharing-Bot'>Click here</a>\n○ Channel : @ratepapreborn\n○ Support Group : https://t.me/joinchat/EheGyCFTvqMwMDk1</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/shoumafitra/File-Sharing-Bot'>Click here</a>\n○ Channel : @ratepapreborn699\n○ Support Group : @ratepaprebornchat</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
